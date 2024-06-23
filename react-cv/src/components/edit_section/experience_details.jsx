29a20914475c10eafe9bc8ebfbab577c6061e33d
Danielius Ryliskis <ryliskisdanielius@gmail.com>
2024-06-23 20:55:16 +0300

    Removed the logic behind creating new component
    Removes the logic behind creating new components and instead adds hide class to the form

--- react-cv/src/components/edit_section/experience_details.jsx
+++ react-cv/src/components/edit_section/experience_details.jsx
@@ -1,12 +1,15 @@
 import AddSVG from "../../assets/add.svg";
 import ExperienceForm from "./experience_form";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function ExperienceDetails() {
-  const [importedForm, setImportedForm] = useState([]);
-  const [nextKey, setNextKey] = useState(0);
+  const [form, setForm] = useState({
+    company: "",
+    role: "",
+    description: "",
+  });
   const [company, setCompany] = useState("");
-  const [role, setRole] = useState("");
+  const [role, setRole] = useState([]);
   const [description, setDescription] = useState("");
 
   const revealExperience = function (e) {
@@ -15,23 +18,24 @@ function ExperienceDetails() {
     document.querySelector(".add-experience").classList.remove("hidden");
   };
   const revealExperienceField = function (e) {
-    e.target.classList.add("hidden");
     document.querySelector(".add-experience").classList.add("hidden");
-    // document.querySelector(".form--experience").classList.remove("hidden");
-    setImportedForm([
-      ...importedForm,
-      {
-        id: nextKey,
-        component: (
-          <ExperienceForm
-            sCompany={setCompany}
-            sRole={setRole}
-            sDescription={setDescription}
-          />
-        ),
-      },
-    ]);
-    setNextKey(nextKey + 1);
+    document.querySelector(".form--experience").classList.remove("hidden");
+  };
+  const changeCompany = function (e) {
+    setCompany(e.target.value);
+    // Parenthesis lets you write an object within an arrow function, otherwise it will think that you're trying to write a multi-line statement
+    setForm((f) => ({ ...f, company: e.target.value }));
+    console.log(form);
+  };
+  const changeRole = function (e) {
+    setRole(e.target.value);
+  };
+  const changeDescription = function (e) {
+    setDescription(e.target.value);
+  };
+  const cancel = function () {};
+  const save = function () {
+    console.log(form);
   };
 
   return (
@@ -49,12 +53,34 @@ function ExperienceDetails() {
           </button>
         </div>
       </div>
-
-      {importedForm.map(({ id, component }) => (
-        <React.Fragment key={id}>{component}</React.Fragment>
-      ))}
-      <p>{company}</p>
-      <p>{role}</p>
+      <div className="form form--experience hidden">
+        <div>
+          <label htmlFor="company">Company:</label>
+          <input
+            type="text"
+            id="company"
+            value={company}
+            onChange={changeCompany}
+          />
+        </div>
+        <div>
+          <label htmlFor="role">Role:</label>
+          <input type="text" id="role" value={role} onChange={changeRole} />
+        </div>
+        <div className="text-area">
+          <label htmlFor="description">Description:</label>
+          <textarea
+            name="description"
+            id="description"
+            value={description}
+            onChange={changeDescription}
+          ></textarea>
+        </div>
+        <div className="cancel-and-save">
+          <button onClick={cancel}>Cancel</button>
+          <button onClick={save}>Save</button>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
--- react-cv/src/components/edit_section/experience_form.jsx
+++ react-cv/src/components/edit_section/experience_form.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function ExperienceForm({ sCompany, sRole, sDescription }) {
+function ExperienceForm({ sCompany, sRole, sDescription, id, del, sav }) {
   const [company, setCompany] = useState("");
   const [role, setRole] = useState("");
   const [description, setDescription] = useState("");
+
+  const object = {
+    key: id,
+    company: company,
+    role: role,
+    description: description,
+  };
 
   const changeCompany = function (e) {
     setCompany(e.target.value);
@@ -11,23 +18,26 @@ function ExperienceForm({ sCompany, sRole, sDescription }) {
   };
   const changeRole = function (e) {
     setRole(e.target.value);
-    sRole(e.target.value);
+    sRole(e.target.value, id);
   };
   const changeDescription = function (e) {
     setDescription(e.target.value);
     sDescription(e.target.value);
   };
 
+  const destroy = function () {
+    del(id);
+    // Fix this later
+  };
+  const save = function () {
+    sav();
+  };
+
   return (
     <div className="form form--experience">
       <div>
         <label htmlFor="company">Company:</label>
-        <input
-          type="text"
-          id="company"
-          value={company}
-          onChange={changeCompany}
-        />
+        <input type="text" id="company" value={id} onChange={changeCompany} />
       </div>
       <div>
         <label htmlFor="role">Role:</label>
@@ -43,8 +53,8 @@ function ExperienceForm({ sCompany, sRole, sDescription }) {
         ></textarea>
       </div>
       <div className="cancel-and-save">
-        <button onClick={removeElement}>Cancel</button>
-        <button onClick={MakeItHidden}>Save</button>
+        <button onClick={destroy}>Cancel</button>
+        <button onClick={save}>Save</button>
       </div>
     </div>
   );
